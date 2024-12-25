0e553806d6fe28d5aeb6f885ea4c59bf12e4da36
installer <installer@gmail.com>
2024-12-25 03:55:07 +0300

    update

--- shell.nix
+++ shell.nix
@@ -4,19 +4,23 @@ pkgs.mkShell {
   name = "bun-env";
 
   # Define the dependencies you need
-  buildInputs = [
+  packages = [
     pkgs.sqlite
     pkgs.bun
+    pkgs.stdenv.cc.cc.lib
   ];
 
   # Setup the Bun environment
   shellHook = ''
-    # Ensure Bun is available in the shell session
-    export PATH="$HOME/.bun/bin:$PATH"
+      env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib];
+
+      # Ensure Bun is available in the shell session
+      export PATH="$HOME/.bun/bin:$PATH"
 
 
-    # Install sqlite3 package with Bun
-    echo "Installing sqlite3 package with Bun..."
-    bun add sqlite3
+      # Install sqlite3 package with Bun
+      echo "Installing sqlite3 package with Bun..."
+      bun add sqlite3
+    bun run dev
   '';
 }
