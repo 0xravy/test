7849119634a9605d2a8386e8a0234498ccf5f2c1
installer <installer@gmail.com>
2024-12-25 04:00:48 +0300

    update

--- shell.nix
+++ shell.nix
@@ -12,15 +12,15 @@ pkgs.mkShell {
 
   # Setup the Bun environment
   shellHook = ''
-      env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib];
+    env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib];
 
-      # Ensure Bun is available in the shell session
-      export PATH="$HOME/.bun/bin:$PATH"
+    # Ensure Bun is available in the shell session
+    export PATH="$HOME/.bun/bin:$PATH"
 
 
-      # Install sqlite3 package with Bun
-      echo "Installing sqlite3 package with Bun..."
-      bun add sqlite3
+    # Install sqlite3 package with Bun
+    echo "Installing sqlite3 package with Bun..."
+    bun add sqlite3
     bun run dev
   '';
 }
