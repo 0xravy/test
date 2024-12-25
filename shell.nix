c1d0b472cb5157e0d947a23d23c3c7b7c38ab43e
installer <installer@gmail.com>
2024-12-25 03:51:04 +0300

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,22 +5,12 @@ pkgs.mkShell {
 
   # Define the dependencies you need
   buildInputs = [
-    pkgs.nodejs
     pkgs.sqlite
-    pkgs.curl
+    pkgs.bun
   ];
 
   # Setup the Bun environment
   shellHook = ''
-    # Install Bun if it's not already installed
-    if ! command -v bun &> /dev/null; then
-      echo "Bun is not installed. Installing Bun..."
-      curl -fsSL https://bun.sh/install | bash
-      export PATH="$HOME/.bun/bin:$PATH"
-    else
-      echo "Bun is already installed."
-    fi
-
     # Ensure Bun is available in the shell session
     export PATH="$HOME/.bun/bin:$PATH"
 
